71168f46cabb81ee5576128e93d02990e3546319
Anders Cui <anders.cui@ef.com>
2015-09-03 03:27:23 +0800

    Add PosSeg.

--- Segmenter.Tests/FCL/TestRegex.cs
+++ Segmenter.Tests/FCL/TestRegex.cs
@@ -13,13 +13,24 @@ namespace Segmenter.Tests.FCL
         [TestCase]
         public void TestMatches()
         {
-            Regex re = new Regex(@"\d+");
+            var re = new Regex(@"\d+");
             var input = "12 34 567";
             var mat = re.Matches(input);
             foreach (Match m in mat)
             {
                 Console.WriteLine("{0} {1}", m.Index, m.Length);
             }
+        }
+
+        [TestCase]
+        public void TestNumbers()
+        {
+            var re = new Regex("[.0-9]+");
+            Assert.That(re.IsMatch("..."));
+            Assert.That(re.IsMatch("123"));
+            Assert.That(re.IsMatch("1.23"));
+            Assert.That(re.IsMatch("1.2.3"));
+            Assert.That(re.IsMatch("a.1.2.3"));
         }
     }
 }
---
+++ docs/todo.md
@@ -0,0 +1,9 @@
+1. PosSeg
+2. Extract Tags
+3. TextRank
+4. Spell check and suggests
+5. Search with Lucene
+
+
+Misc
+1.
