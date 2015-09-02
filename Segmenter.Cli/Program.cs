1f1d63884bebbedd352a6a1d763fd1f4cd4f4211
Anders Cui <anders.cui@ef.com>
2015-09-02 15:32:54 +0800

    Add jieba.NET simple command line tool.

---
+++ Segmenter.Cli/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace JiebaNet.Segmenter.Cli
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("No file specified");
+                return;
+            }
+
+            var result = new List<string>();
+
+            var filename = Path.GetFullPath(args[0]);
+            var lines = File.ReadAllLines(filename);
+
+            var segmenter = new JiebaSegmenter();
+            foreach (var line in lines)
+            {
+                result.Add(string.Join("/ ", segmenter.Cut(line)));
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, result));
+        }
+    }
+}
--- Segmenter.Tests/FCL/TestIO.cs
+++ Segmenter.Tests/FCL/TestIO.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Text;
 using NUnit.Framework;
 
 namespace Segmenter.Tests.FCL
@@ -9,42 +7,15 @@ namespace Segmenter.Tests.FCL
     [TestFixture]
     public class TestIO
     {
-        private static readonly string ProbFilePath = "../../prob.txt";
-
-        [TestCase]
-        public void TestReadFile()
-        {
-            var lines = File.ReadAllLines(ProbFilePath, Encoding.UTF8);
-
-            IDictionary<char, IDictionary<char, double>> emit = new Dictionary<char, IDictionary<char, double>>();
-            IDictionary<char, double> values = null;
-            foreach (var line in lines)
-            {
-                var tokens = line.Split('\t');
-                if (tokens.Length == 1)
-                {
-                    values = new Dictionary<char, double>();
-                    emit[tokens[0][0]] = values;
-                }
-                else
-                {
-                    values[tokens[0][0]] = double.Parse(tokens[1]);
-                }
-            }
-
-            Console.WriteLine(emit.Count);
-            Console.WriteLine(values.Count);
-        }
-
         [TestCase]
         public void TestNormalizePath()
         {
             var p = @"..\test.txt";
-            Console.WriteLine(Path.IsPathRooted(p));
+            Assert.That(Path.IsPathRooted(p));
             Console.WriteLine(Path.GetFullPath(p));
 
             p = @"C:\test.txt";
-            Console.WriteLine(Path.IsPathRooted(p));
+            Assert.That(Path.IsPathRooted(p), Is.False);
             Console.WriteLine(Path.GetFullPath(p));
         }
     }
